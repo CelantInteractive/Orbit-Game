82073f139a089f3177842174b9afedb967213e8c
White <monsieur_white@hotmail.com>
2015-11-28 16:07:53 +1030

    Added Unbind

---
+++ Assets/Scripts/Commands/BindCommands.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Commands
+{
+	public partial class CommandManager
+	{
+		private List<char> binds = new List<char>();
+
+		public CommandResult Bind(char key)
+		{
+			Debug.Log("BINDING KEY: " + key);
+			if (!binds.Contains(key))
+			{
+				binds.Add(key);
+				return CommandResult.Success;
+			}
+
+			return CommandResult.Failure;
+		}
+
+		public CommandResult Unbind(char key)
+		{
+			Debug.Log("UNBINDING KEY: " + key);
+			if (!binds.Contains(key))
+			{
+				return CommandResult.Failure;
+			}
+
+			binds.Remove(key);
+			return CommandResult.Success;
+		}
+	}
+}
